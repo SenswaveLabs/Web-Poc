bcf99b4a8ff77b94d8eb85546ba5e9e954026b61
Łukasz Kolber <69080157+psp515@users.noreply.github.com>
2026-03-17 21:42:13 +0100

    feat: device form (#55)

---
+++ src/Modules/Senswave.Web.Devices/Models/DeviceModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Senswave.Web.Devices.Models;
+
+public class DeviceModel
+{
+    public string Id { get; set; } = string.Empty;
+    public string HomeId { get; set; } = string.Empty;
+    public string RoomId { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+    public string Icon { get; set; } = "bulb-outline";
+
+    // Tile properties
+    public string TileType { get; set; } = "Default";
+    public string TileOperationId { get; set; } = string.Empty;
+}
